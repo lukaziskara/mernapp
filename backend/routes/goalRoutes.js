2b13e83ab27e06d16b5115198da0d87c2846c561
Luka Tsiskarishvili <lukaziskara@gmail.com>
2022-12-21 15:18:03 +0400

    backend, MERN stack, part II

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,8 +6,10 @@ const { getGoals,
 	deleteGoal 
 } = require('../controllers/goalControllers')
 
-router.route('/').get(getGoals).post(setGoal)
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 module.exports = router
